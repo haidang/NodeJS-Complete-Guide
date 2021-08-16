221d9473e647418db9cb36136b6954f8c7c6ee96
Dang Thach <nvanhaidang@gmail.com>
2021-08-16 09:19:26 +0700

    15. Working with Objects, Properties & Methods

--- play.js
+++ play.js
@@ -1,32 +1,61 @@
-const name = 'Max';
-let age = 29;
-const hasHobbies = true;
-
-// name = 'Dang Thach'; // will be error
-age = 30;
-
-// console.log(name);
-
-const summarizeUser = (userName, userAge, userHasHobby) => {
-  return (
-    'Name is ' +
-    userName +
-    ', age is ' +
-    userAge +
-    ' and the user has hobbies: ' +
-    userHasHobby
-  );
+// A key-value pair is also called a "property" or a "field" of the object
+const person = {
+  name: 'Max',
+  age: 29,
+  greet() {
+    console.log('Hi, I am ' + this.name); // => "Hi, I am Max"
+  },
 };
+person.greet(); // => "Hi, I am Max"
 
-// console.log(summarizeUser(name, age, hasHobbies));
-
-// const add = (a, b) => {
-//   return a + b;
+// const person = {
+//   name: 'Max',
+//   age: 29,
+//   greet: function () {
+//     console.log('Hi, I am ' + this.name);
+//   },
 // };
-const add = (a, b) => a + b;
-const addOne = (a) => a + 1;
-const addRandom = () => 1 + 2;
+// person.greet(); // => "Hi, I am Max"
 
-console.log(addRandom());
-// console.log(addOne(5));
-// console.log(add(1, 2));
+// const person = {
+//   name: 'Max',
+//   age: 29,
+//   greet: () => {
+//     console.log('Hi, I am ' + this.name);
+//   },
+// };
+// person.greet(); // => "Hi, I am undefined"
+// console.log(person);
+
+// const name = 'Max';
+// let age = 29;
+// const hasHobbies = true;
+
+// // name = 'Dang Thach'; // will be error
+// age = 30;
+
+// // console.log(name);
+
+// const summarizeUser = (userName, userAge, userHasHobby) => {
+//   return (
+//     'Name is ' +
+//     userName +
+//     ', age is ' +
+//     userAge +
+//     ' and the user has hobbies: ' +
+//     userHasHobby
+//   );
+// };
+
+// // console.log(summarizeUser(name, age, hasHobbies));
+
+// // const add = (a, b) => {
+// //   return a + b;
+// // };
+// const add = (a, b) => a + b;
+// const addOne = (a) => a + 1;
+// const addRandom = () => 1 + 2;
+
+// console.log(addRandom());
+// // console.log(addOne(5));
+// // console.log(add(1, 2));
