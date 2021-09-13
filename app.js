eccff82eb59ff173fd378568bb795f957b828a5b
Dang Thach <nvanhaidang@gmail.com>
2021-09-13 21:38:32 +0700

    59. Installing Express.js

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const http = require('http');
 
+const express = require('express');
+const app = express();
+
 const routes = require('./routes');
 
 console.log(routes.someText);
-const server = http.createServer(routes.handler);
+const server = http.createServer(app);
 
 server.listen(3000);
